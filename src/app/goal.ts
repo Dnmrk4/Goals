765c40e990814c86d519e461d4c5cc23bab011b7
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-02 23:11:01 +0300

    added

---
+++ src/app/goal.ts
@@ -0,0 +1,6 @@
+export class Goal {
+  public showDescription: boolean;
+  constructor(public id: 0011, public name: "ToDo", public description: "To do list") {
+    this.showDescription = false
+  }
+}
